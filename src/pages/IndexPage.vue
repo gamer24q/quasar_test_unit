48cbcea5527e8b4fcd99e755ace95af91f5b4b2c
gamer24q <pannawattonkham24@gmail.com>
2023-10-27 16:04:48 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage />
+  <FunctionPage />
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  components:{
+    InputPage,
+    FunctionPage
+  },
+  data() {
+    return {
+      title: 'พูนศักดิ์ กันแก้ว.',
+    }
+  },
+}
+
+import InputPage from './InputPage.vue';
+import FunctionPage from './FunctionPage.vue'
+</script>
+
+<style scoped></style>
